05e14d1e48670f50eac1476bce541638cc6f0ac2
KARL <KARL@TINGUME-PC>
2016-08-15 19:27:51 +0100

    test checkin

---
+++ application/views/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Link bootstrap to the page -->
+        <!-- Bootstrap Core CSS -->
+        <link href= "<?php echo base_url('resources/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    
+    
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div id="navigation" class="col-lg-2 col-md-3 col-sm-3">
+                    <li>
+                    <u>Lecturers</u>
+                    </li>
+                </div>
+            </div>
+        </div>       
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url('resources/js/bootstrap.min.js'); ?>"</script>
+    </body>
+</html>
