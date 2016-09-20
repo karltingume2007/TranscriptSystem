d3bc30bfc231c9c3803a1077e33569430c6e4d78
ebuneachancho <ebuneachancho@gmail.com>
2016-09-20 17:14:04 +0100

    Updated setting controller via upload

--- application/controllers/setting.php
+++ application/controllers/setting.php
@@ -5,7 +5,41 @@ class setting extends MY_Controller {
     public function index()
     {
         $this->LoadViewHeader();
-        $this->load->view('school_configuration/settings');
-        $this->LoadViewFooter();
+        $this->load->view('school_configuration/settings', array('error' => ' ' ));
+        $this->LoadViewFooter('school_configuration/settings_js');
     }
+   	
+    // function that gets school name and logo from db
+   	public function getData(){
+   		  //validate form input
+        $this->form_validation->set_rules('name', 'Name', 'trim|required');
+        $this->form_validation->set_rules('logo', 'School logo', 'callback__image_upload');
+        
+
+        if ($this->form_validation->run() == true){       
+
+         $config['upload_path']   = './uploads/'; 
+         $config['allowed_types'] = 'gif|jpg|png'; 
+         $config['max_size']      = 100; 
+         $config['max_width']     = 1024; 
+         $config['max_height']    = 768;  
+         $this->load->library('upload', $config);
+            
+         if ( ! $this->upload->do_upload('userfile')) {
+            $error = array('error' => $this->upload->display_errors()); 
+            $this->load->view('upload_form', $error); 
+         }
+            
+         else { 
+            $data = array('upload_data' => $this->upload->data()); 
+         } 
+        
+    }else{
+        $validate = array('error' => validation_errors());
+            $this->session->set_flashdata($validate);
+            // redirect('setting');
+    }
+
+
+   	}
 }
