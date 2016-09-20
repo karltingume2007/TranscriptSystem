442c2488dd6244ff5f02384cd0181c9f3a3aa7a3
ebuneachancho <ebuneachancho@gmail.com>
2016-09-20 17:16:41 +0100

    Updated settings view

--- application/views/school_configuration/settings.php
+++ application/views/school_configuration/settings.php
@@ -1,21 +1,29 @@
-<div class="container fluid">
-	<div class="col-xs-2"></div>
+<div  ng-controller="SchoolConfigController as controller">
+   <div class="col-xs-2"></div>
 
 	<div class="col-xs-7" style="border:1px solid #ccc; padding:3% 1% 3% 1%;">
-		<form method="post" action="">
+
+		<?php echo $error;?>
+		<?php echo form_open_multipart('');?>
 			<div class="form-group">
 				<label for="Name of School">School:</label>
-				<input type="text" name="school" value="Fomic Polytechnic" class="form-control">
+				<input type="text" name="school" class="form-control">
 			</div>
 			<div class="form-group">
 				<label for="School Logo">Logo: </label>
-				<input type="file" name="logo">
+				<input type="file" file-model = "myFile">
 				<!-- Add Logic that accepts only a picture format -->
 			</div>
 			<br>
-  			<button type="submit" class="btn btn-primary btn-block">Submit</button>
+  			<div class="form-group">
+  				<button type="submit" ng-click = "uploadFile()" class="btn btn-primary btn-block">Submit</button>
+  			</div>
+  			
 		</form>
 	</div>
 
-	<div class="col-xs-3"></div>
+	<div class="col-xs-3">
+		
+	</div>
+
 </div>
