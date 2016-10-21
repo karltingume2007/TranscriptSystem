4501793950894571cc14abf438916b81f002c791
ebuneachancho <ebuneachancho@gmail.com>
2016-10-21 07:52:41 -0700

    Add files via upload
    Updated the settings controller

--- application/controllers/setting.php
+++ application/controllers/setting.php
@@ -1,45 +1,38 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class setting extends MY_Controller {
+    function __construct(){
+        parent::__construct();
+    }
 
     public function index()
     {
         $this->LoadViewHeader();
-        $this->load->view('school_configuration/settings', array('error' => ' ' ));
-        $this->LoadViewFooter('school_configuration/settings_js');
-    }
-   	
-    // function that gets school name and logo from db
-   	public function getData(){
-   		  //validate form input
-        $this->form_validation->set_rules('name', 'Name', 'trim|required');
-        $this->form_validation->set_rules('logo', 'School logo', 'callback__image_upload');
-        
-
-        if ($this->form_validation->run() == true){       
-
-         $config['upload_path']   = './uploads/'; 
-         $config['allowed_types'] = 'gif|jpg|png'; 
-         $config['max_size']      = 100; 
-         $config['max_width']     = 1024; 
-         $config['max_height']    = 768;  
-         $this->load->library('upload', $config);
-            
-         if ( ! $this->upload->do_upload('userfile')) {
-            $error = array('error' => $this->upload->display_errors()); 
-            $this->load->view('upload_form', $error); 
-         }
-            
-         else { 
-            $data = array('upload_data' => $this->upload->data()); 
-         } 
-        
-    }else{
-        $validate = array('error' => validation_errors());
-            $this->session->set_flashdata($validate);
-            // redirect('setting');
+        $this->load->view('school_configuration/settings');
+        $this->LoadViewFooter();
     }
 
+    public function save(){
+        $logo = $this->do_upload();
+        $school_name = $this->input->post('school', 'School Name');
+        $this->load->model('system_setting');
+        $data = array('schoolName' => $school_name, 'schoolLogo' => $logo);
+        $saveSettings = $this->system_setting->changeSchoolName($data);
+       redirect('setting');
+    }
+    // function that handles the image upload
+    private function do_upload(){
+        $type = explode('.', $_FILES["logo"]["name"]);
+        $type = $type[count($type)-1];
+        $url = "./resources/uploads/". uniqid(rand()). '.' . $type;
+        if (in_array($type, array("jpg", "jpeg", "png", "gif"))) {
+            if (is_uploaded_file($_FILES["logo"]["tmp_name"])) {
+                if ( move_uploaded_file($_FILES["logo"]["tmp_name"], $url)){
+                    return $url;
+                }
+        }
+        }
+        
+    }
 
-   	}
 }
